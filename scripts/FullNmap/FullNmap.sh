16eed2e50f326b86517368e25b90fde9b8a8e16b
Iuri Moro <iuribpmoro@gmail.com>
2021-05-08 12:13:01 -0300

    Scripts updated and styled

--- scripts/FullNmap/FullNmap.sh
+++ scripts/FullNmap/FullNmap.sh
@@ -1,14 +1,47 @@
 #!/bin/bash
 
+# ----------------------------- Color definition ----------------------------- #
+
+black=`tput setaf 0`
+red=`tput setaf 1`
+green=`tput setaf 2`
+yellow=`tput setaf 3`
+blue=`tput setaf 4`
+magenta=`tput setaf 5`
+cyan=`tput setaf 6`
+white=`tput setaf 7`
+bold=`tput bold`
+reset=`tput sgr0`
+
+randomColorNumber=$((0 + $RANDOM % 7))
+randomColor=`tput setaf ${randomColorNumber}`
+
 printBanner () {
-    echo -e "\n"
+    echo -e "\n${randomColor}${bold}"
     echo "███████╗██╗   ██╗██╗     ██╗     ███╗   ██╗███╗   ███╗ █████╗ ██████╗ "
     echo "██╔════╝██║   ██║██║     ██║     ████╗  ██║████╗ ████║██╔══██╗██╔══██╗"
     echo "█████╗  ██║   ██║██║     ██║     ██╔██╗ ██║██╔████╔██║███████║██████╔╝"
     echo "██╔══╝  ██║   ██║██║     ██║     ██║╚██╗██║██║╚██╔╝██║██╔══██║██╔═══╝ "
     echo "██║     ╚██████╔╝███████╗███████╗██║ ╚████║██║ ╚═╝ ██║██║  ██║██║     "
     echo "╚═╝      ╚═════╝ ╚══════╝╚══════╝╚═╝  ╚═══╝╚═╝     ╚═╝╚═╝  ╚═╝╚═╝     "
+    echo -e "\n${reset}"
+}
+
+# -------------------------------- Ping Sweep -------------------------------- #
+
+pingSweep () {
+
+    target=$1
+    network=$2
+
     echo -e "\n"
+    echo "${blue}${bold}--------- RUNNING PING SWEEP ON THE $target NETWORK ---------${reset}"
+
+    sudo nmap -sn -PE $target | grep report | cut -d ' ' -f5 > hosts
+
+    echo "${bold}Hosts identificados:${reset}"
+    cat hosts
+
 }
 
 networkScan () {
@@ -18,38 +51,14 @@ networkScan () {
     network=`echo $target | cut -d "/" -f1`
     mask=`echo $target | cut -d "/" -f2`
 
-    printBanner
-
     mkdir $network
+    cd $network
     
-    echo -e "\n"
-    echo "--------- RUNNING PING SWEEP ON THE $target NETWORK ---------"
+    pingSweep $target $network
 
-    nmap -sn -PE $target | grep report | cut -d ' ' -f5 > $network/hosts
-
-    echo "Hosts identificados:"
-    cat $network/hosts
-
-    echo -e "\n"
-    echo "--------- RUNNING PORT SCAN ON THE HOSTS ---------"
-
-    for host in $(cat $network/hosts)
+    for host in $(cat hosts)
     do
-        mkdir $network/$host
-        
-        echo "Open ports on host $host:"
-
-        nmap -T4 -p- -Pn $host 2> /dev/null | grep 'open\|closed\|filtered\|unfiltered' | grep -v ':' | grep -v 'All' | cut -d ' ' -f1 | cut -d '/' -f1 > $network/$host/openPorts
-        cat $network/$host/openPorts
-
-        echo "Running full analysis of the ports..."
-    	nmap -A -sV -Pn -p $(tr '\n' , < $network/$host/openPorts) $host > $network/$host/scanResult 2> /dev/null
-
-        wait
-        
-        echo "Scan on host $host finished!"
-        echo -e "\n"
-
+        hostScan $host
     done
 
     echo "Scan completed successfully!"
@@ -59,42 +68,41 @@ hostScan () {
     
     target=$1
 
-    printBanner
-
     mkdir $target
     
-    echo -e "\n"
-    echo "--------- RUNNING PORT SCAN ON THE HOST ---------"
+    echo -e "\n${blue}${bold}--------- SCANNING TARGET $target ---------${reset}"
         
-    echo "Open ports on host $target:"
+    echo -e "${bold}Open ports on host $target:${reset}"
 
     nmap -T4 -p- -Pn $target 2> /dev/null | grep 'open\|closed\|filtered\|unfiltered' | grep -v ':' | grep -v 'All' | cut -d ' ' -f1 | cut -d '/' -f1 > $target/openPorts
     cat $target/openPorts
 
-    echo "Running full analysis of the ports..."
-    nmap -A -sV -Pn -p $(tr '\n' , < $target/openPorts) $target > $target/scanResult 2> /dev/null
-
-    wait
+    echo -e "\n${bold}Running full analysis of the ports...${reset}"
+    nmap -A -Pn -p $(tr '\n' , < $target/openPorts) $target > $target/scanResult 2> /dev/null
+    cat $target/scanResult | grep http | grep 'open\|closed\|filtered\|unfiltered' | cut -d " " -f1 | cut -d "/" -f1 > $target/httpPorts
+    echo -e "${green}Scan results saved on $target/scanResult ${reset}\n"
     
-    echo "Scan on host $target completed!"
+    echo -e "\n${green}${bold}Scan on host $target completed!${reset}"
 
-
-    echo "Scan completed successfully!"
 }
 
 if [ "$1" == "" -o "$2" == "" ]
 then
     printBanner
-	echo "Usage: $0 <MODE> <NETWORK/MASK>"
-	echo "Example: $0 network 192.168.0.0/24"
+    
+	echo -e "${bold}Usage: ${reset}./Automap.sh <MODE> <NETWORK/MASK>"
+	echo -e "${bold}Example: ${reset}./Automap.sh network 192.168.0.0/24"
     echo -e "\n"
 
-    echo "Available Modes: "
-    echo -e "\thost - scans a specific host"
-    echo -e "\tnetwork - scans all hosts found in a network"
+    echo -e "${bold}Available Modes: ${reset}"
+    echo -e "\thost: scans a specific host"
+    echo -e "\tnetwork: scans all hosts found in a network"
 
     echo -e "\n"
 else
+    
+    printBanner
+    
     target=$2
 
     if [ "$1" == "host" ]
--- scripts/NmapCutter/NmapCutter.sh
+++ scripts/NmapCutter/NmapCutter.sh
@@ -1,13 +1,29 @@
 #!/bin/bash
 
+# ----------------------------- Color definition ----------------------------- #
+
+black=`tput setaf 0`
+red=`tput setaf 1`
+green=`tput setaf 2`
+yellow=`tput setaf 3`
+blue=`tput setaf 4`
+magenta=`tput setaf 5`
+cyan=`tput setaf 6`
+white=`tput setaf 7`
+bold=`tput bold`
+reset=`tput sgr0`
+
+randomColorNumber=$((0 + $RANDOM % 7))
+randomColor=`tput setaf ${randomColorNumber}`
+
 if [ "$1" == "" ]
 then
-    echo "Usage: $0 scanResultsFile"
-    echo "Example: $0 ./results"
+    echo -e "${bold}\nUsage: ${reset}./NmapCutter.sh scanResultsFile"
+    echo -e "${bold}Example: ${reset}./NmapCutter.sh ./results"
     
     echo -e "\n"
 
-    echo "Obs.: Testado apenas para quick scans!"
+    echo "${bold}Obs.: ${reset}Testado apenas para quick scans!"
 
     echo -e "\n"
 else
